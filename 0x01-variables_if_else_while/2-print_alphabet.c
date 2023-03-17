2f45c90c0f6b4d5e6a07c61c8228a006ad38cffc
Infrixxx <120354783+Infrixxx@users.noreply.github.com>
2023-03-17 21:40:51 +0200

    3-print_alphabets.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,12 +12,14 @@
  */
 int main(void)
 {
-char alphabet = 'a';
-while (alphabet <= 'z')
+        char alphabet = 'a';
+        
+        while (alphabet <= 'z')
         {
                 putchar(alphabet);
                 alphabet++;
         }
+        
         putchar('\n');
         return (0);
 }
