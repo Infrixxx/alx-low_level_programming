2758de6574335b6600e6660b355c79e21860fabe
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-19 10:50:25 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,14 +15,14 @@ int main(void)
 int num = 0;
 char letter = 'a';
 
-while ( num < 10)
+while (num < 10)
 {
 	putchar((num % 10) + '0');
-	num++;}
+	num++; }
 
 while ( letter <= 'f')
 {	putchar(letter);
-	letter++;}
+	letter++; }
 
 putchar('\n');
 
