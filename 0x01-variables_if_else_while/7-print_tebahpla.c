66c1b6971a686855a3601dc6d82358c315382e18
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-19 10:31:59 +0200

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,26 +1,23 @@
 /*
  * File: 7-print_tebahpla.c
-<<<<<<< HEAD
- * Auth: Brennan D Baraban
-=======
  * Auth: Boitumelo C Rachoshi
  */
 
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+/* 7. Smile in the mirror*/
 
-/**
- * main - Prints the lowercase alphabet in reverse.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-	char letter;
-
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
-
-	putchar('\n');
-
-	return (0);
+    char lt='z';
+    
+    while(lt>'a'-1)
+    {
+    putchar(lt);
+    lt--;
+    }
+    
+    putchar('\n');
+    return(0);
 }
