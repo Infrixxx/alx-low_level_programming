1ae112a9f73dc41875d556f5cf212aa17a953bc3
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-19 23:37:58 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,8 @@
  */
 int main(void)
 { /* Declarations*/
-    int d1;
-    int d2;
+int d1;
+int d2;
 
 
 for (d1=0;d1<9;d1++)
@@ -24,13 +24,13 @@ for (d2=d1+1;d2<10;d2++)
 putchar(d1+'0');
 putchar(d2+'0');
 if (d1==8 && d2==9)
-	continue;
+continue;
 putchar (',');
 putchar (' ');
 }
 
 }
-            
+
 
 putchar ('\n');
 
