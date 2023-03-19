084062f6eef75ef357c2bc5984c1258e8217ab4b
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-20 00:42:23 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,7 +14,8 @@ for (d1=0;d1<9;d1++)
 {for(d2=d1+1;d2<10;d2++)
 {for(d3=d2+1;d3<10;d3++)
 
-{putchar(d1+'0');
+{
+putchar(d1+'0');
 putchar(d2+'0');
 putchar(d3+'0');
 if (d1 == 7 && d2 == 8 && d3 == 9)
