1170dbd8c57310b4c0bd5656b16c31133b15741b
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-19 23:29:14 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,22 +12,22 @@
  * Return: Always 0.
  */
 int main(void)
-{
-	int digit1, digit2;
+{ /* Declarations*/
+    int d1;
+    int d2;
+    
 
-	for (digit1 = 0; digit1 < 9; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+        for (d1=0;d1<9;d1++)
+        {   
+            for(d2=d1+1;d2<10;d2++)
+            {  
+               putchar(d1+'0');
+               putchar(d2+'0');
+               putchar(',');
+            }
 
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
-	}
+        }
+            
 
 	putchar('\n');
 
