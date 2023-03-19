2fbf9ac894d06c493b4004ae5b88114a7552c21f
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-20 00:58:42 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 /**
-* main - Prints all possible combinations of two different digits,
+* main - Prints all possible combinations of three different digits,
 *        in ascending order, separated by a comma followed by a space.
 *
 * Return: Always 0.
