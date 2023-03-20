20ee4ef5fcbe833151210cfe844d93bad1970581
Boitumelo <bcrachoshi@gmail.com>
2023-03-19 22:33:21 -0700

    Mandatory

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/*
+ *
+ *Author  : BC Rachoshi
+ *Date : 20/03/2023
+ *
+ *0-putchar.c
+ */
+
+/*
+ * program that prints _putchar, followed by a new line
+ *
+ *
+ * main is the entry point */
+
+int main(void)
+{
+	puts("_putchar")
+		return(0)
+}
+
