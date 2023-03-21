8abe0de9dd7f464f5a554bd1aa2ceca4928035c5
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-21 13:11:41 +0200

    Mandatory

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,8 +4,7 @@
  */
 #include "main.h"
 /**
- * jack_bauer- prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
- * @n : The number of which the last digit will be printed.I
+ *jack_bauer- prints every minute of the day
  *
  */
 void jack_bauer(void)
