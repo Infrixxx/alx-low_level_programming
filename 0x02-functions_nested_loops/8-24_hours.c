e03ccbf1c9a9ba05044164b273389444ed0e4fb6
Boitumelo <bcrachoshi@gmail.com>
2023-03-21 03:26:22 -0700

    Mandatory

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+/*
+ * File: 8-24_hours.c
+ * Auth: Boitumelo Rachoshi
+ */
+#include "main.h"
+/**
+ * jack_bauer- prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * @n : The number of which the last digit will be printed.I
+ *
+ */
+void jack_bauer(void)
+{
+	    int h1;
+    int h2;
+    int m1;
+    int m2;
+    
+
+for(h1=0;h1<=2;h1++)
+{
+     
+     for(h2=0;h2<=9;h2++)
+     {
+         for(m1=0;m1<=5;m1++)
+         {
+             for (m2=0;m2<=9;m2++)
+             {
+                    _putchar(h1+'0');  
+                    _putchar(h2+'0');
+                    _putchar(':');
+                    _putchar(m1+'0');  
+                    _putchar(m2+'0');
+                    _putchar('\n');
+                    if (h1==2 && h2==3)
+                        break;
+             }
+         }
+
+      
+}
+}
+}
