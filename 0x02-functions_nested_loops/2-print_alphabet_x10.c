190739ed95fbc8ead8ce475bf48343dfbd48af8d
Boitumelo <bcrachoshi@gmail.com>
2023-03-20 19:57:12 -0700

    Mandatory

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/*
+ * Author:BC Rachoshi
+ *2-print_alphabet_x10.c
+ *
+ */
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+	char a='a';
+
+	for(n=0;n<11;n++)
+		{
+			while char(a<=z)
+			{
+				_putchar('a');
+				a++;
+			}
+		}
+	putchar('\n');
+}
