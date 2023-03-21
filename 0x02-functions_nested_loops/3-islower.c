2ccd7af9d61b83c077acb97a01a948f6a41cfccc
Boitumelo <bcrachoshi@gmail.com>
2023-03-20 22:39:29 -0700

    Revert "Mandatory"
    This reverts commit 1e6749dc95be89085fefd165a395ac391c1b50a5.
    
    Undo

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,2 @@
 #include "main.h"
-/*
- * Author : BC Rachoshi
- *3-islower.c
- *
- * /
 
-/**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
- */
-
-int _islower(int c)
-{
-	return (c >= 'a' && c <= 'z');
-}
