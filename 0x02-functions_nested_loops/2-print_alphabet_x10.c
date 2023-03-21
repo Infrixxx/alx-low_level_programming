8402df78950851e6a29b3739b2961d3348630949
Boitumelo <bcrachoshi@gmail.com>
2023-03-20 20:00:32 -0700

    Mandatory

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,9 +15,9 @@ void print_alphabet_x10(void)
 	int n;
 	char a='a';
 
-	for(n=0;n<11;n++)
+	for (n = 0; n < 11; n++)
 		{
-			while char(a<=z)
+			while ("a" <= "z")
 			{
 				_putchar('a');
 				a++;
