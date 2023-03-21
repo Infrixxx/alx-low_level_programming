7aa23a64f7c5b29849feb6b67ef0b6579e2a93a7
Boitumelo <bcrachoshi@gmail.com>
2023-03-20 23:05:04 -0700

    Mandatory

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+/*
+ *Auth : BC Rachoshi
+ *
+ * 4-isalpha.c
+ */
+
+/*
+ *Write a function that checks for alphabetic character.
+ *
+ * Returns 1 if c is a letter, lowercase or uppercase
+ * Returns 0
+ * */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c => 'A' && c <= 'Z' )
+		return (1)
+	else
+		return (0)
+}
