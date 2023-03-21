2e8ae5244b777cc0d58c155ee1b90791636ae4c3
Boitumelo <bcrachoshi@gmail.com>
2023-03-21 01:54:14 -0700

    Mandatory

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,11 @@
  */
 int print_last_digit(int n)
 {
-	int l = n%10;
-	if (l < 0)
-		l*= -1;
+	int ld = n%10;
+	if (ld < 0)
+		ld*= -1;
 
-	_putchar(l+'0');
-	return(l);
+	_putchar(ld+'0');
+
+	return(ld);
 }
