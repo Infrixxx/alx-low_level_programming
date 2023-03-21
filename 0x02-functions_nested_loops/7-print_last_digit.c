f4caff1651ccf862ffd825f50bf73f5b4f3f0835
Boitumelo <bcrachoshi@gmail.com>
2023-03-21 01:37:36 -0700

    Mandatory

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Boitumelo Rachoshi
+ */
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n : The number of which the last digit will be printed.
+ *
+ * Return : value of the last digit
+ */
+int print_last_digit(int)
+{
+	int n;
+	n=(mod%10);
+	return(n);
+}
