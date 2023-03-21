793a73ecbb78e8117cdbaa8956c091d80622aaf8
Boitumelo <bcrachoshi@gmail.com>
2023-03-21 01:21:47 -0700

    Mandatory

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+/*
+ * Auth :Boitumelo Rachoshi
+ * File :6-abs.c
+ *
+ */
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs(int) - computes the absolute value of an integer
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ *
+int _abs(int)
+	{ int n;
+		if (n >= 0)
+		return (n);
+	else
+		return (-n);
+	}
