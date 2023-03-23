fcadd6f1231439aa7a538b93f4795c5618a793a4
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-23 14:51:20 +0200

    Mandatory

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,13 @@
  * Auth : BC Rachoshi
  * File : 0-isupper.c
  */
-
 #include "main.h"
-
 /**
 * _isupper - Checks for uppercase characters.
 * @c: The character to be checked.
 *
 * Return: 1 if character is uppercase, 0 otherwise.
 */
-
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
