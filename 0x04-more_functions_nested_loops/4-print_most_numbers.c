728cf6ff782a6ec8c64de28f238d30739b9697e4
Boitumelo <bcrachoshi@gmail.com>
2023-03-23 12:55:28 -0700

    mandatory

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-print_most_numbers.c
+ * Auth: Boitumelo Rachoshi
+ */
+
+#include "main.h"
+
+/**
+ * print_most_numbers -prints the numbers, from 0 to 9
+ * 
+ * 
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		if (a = 2 || a = 4)
+				continue;
+		_putchar(j);
+	}
+	_putchar('\n');
+}
