ac2efb06e33884a09acb1400a6d75fee24df6816
Boitumelo <bcrachoshi@gmail.com>
2023-03-23 15:23:01 -0700

    Advanced

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,65 +1,26 @@
-/*
- * File: 101-print_number.c
- * Auth: Boitumelo Rachoshi
- */
+#include "main.h"
 
-include "main.h"
 /**
- * print_number - Function that prints an integer.
- * @n: int type number
- * Description: Can only use _putchar to print.
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
-<<<<<<< HEAD
-	long m; /* power of 10 */
-	int c; /* boolean check */
-	long num; /* convert int to long */
-=======
-	unsigned int num = n;
->>>>>>> 63dc05d11e75a3b99ed435b3a8e1fc78555dc9c0
+	unsigned int n1;
 
-	num = n;
-	/* negatives */
-	if (num < 0)
+	if (n < 0)
 	{
-		num *= -1;
+		n0 = -n;
 		_putchar('-');
-<<<<<<< HEAD
-	}
-
-	/* count up */
-	m = 1;
-	c = 1;
-	while (c)
+	} else
 	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
+		n0 = n;
 	}
 
-	/* count down */
-	while (num >= 0)
+	if (n0 / 10)
 	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
-=======
-		num = -num;
+		print_number(n0 / 10);
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
->>>>>>> 63dc05d11e75a3b99ed435b3a8e1fc78555dc9c0
+	_putchar((n0 % 10) + '0');
 }
