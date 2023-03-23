de061c5061ad3f1b8a2111e6aec55165e3ebf1af
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-23 23:21:32 +0200

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,7 +10,6 @@ int main(void)
 {
 	int c;
 	long num = 612852475143;
-
 	for (c = (int) sqrt(num); c > 2; c++)
 	{
 		if (num % c == 0)
@@ -19,6 +18,5 @@ int main(void)
 			break;
 		}
 	}
-
 	return (0);
 }
