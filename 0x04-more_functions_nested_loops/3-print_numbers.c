212d163d2c0f71c97472a922793ccd9b6e4d9524
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-23 21:33:32 +0200

    Mandatory

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,9 +6,9 @@
 #include "main.h"
 
 /**
- * print_numbers -prints the numbers, from 0 to 9
- * 
- * Return: ALways 0.
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
