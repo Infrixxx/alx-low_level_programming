1618f9205792f689bed7cd7cfc346bfb1f19ccee
Boitumelo <bcrachoshi@gmail.com>
2023-03-23 04:24:00 -0700

    mandatory

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,11 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
-    else if (b > a && b > c)
+    else if (b > a)
     {
         largest = b;
     }
-    else
+    elsie
     {
         largest = c;
     }
