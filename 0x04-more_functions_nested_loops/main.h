8e8866d59035ffbaebebcfbd08562754b48163c7
Boitumelo <bcrachoshi@gmail.com>
2023-03-23 10:51:07 -0700

    mandatory

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 /*
- * File: holberton.h
+ * File: main.h
  * Auth: BC Rachoshi
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x04-more_functions_nested_loops directory.
