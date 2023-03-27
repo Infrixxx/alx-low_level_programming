bfc90c1e34d50b83fb8dca9a449f60b1c009fed6
Boitumelo <bcrachoshi@gmail.com>
2023-03-27 13:31:05 -0700

    mandatory

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,7 +7,7 @@
 /**
  * reset_to_98 - updates the value of the variable
  * pointer points to to 98
- * @n: pointer to the variable to update
+ * @n : pointer to the variable to update
  */
 void reset_to_98(int *n)
 {
