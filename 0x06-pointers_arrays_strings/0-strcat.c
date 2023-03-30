743f1c1d314115339a3a0ab5931b41877a5e05e8
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-03-30 11:01:37 +0200

    Mandatory

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,20 +13,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a,b;
+int a,b;
 
-	a = 0;
-	while (dest[a] != '\0')
-	{
-		a++;
-	}
-	b = 0;
-	while (src[b] != '\0')
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
-	dest[a] = '\0';
-	return (dest);
+a = 0;
+while (dest[a] != '\0')
+{
+a++;
+}
+b = 0;
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
 }
