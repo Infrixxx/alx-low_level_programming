66528f6639bbbcf4f384a8748cd5b28a91d2435b
Boitumelo <bcrachoshi@gmail.com>
2023-03-30 08:44:09 -0700

    Mandatory

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,10 @@ char *string_toupper(char *n)
 int a;
 a = 0;
 while (n[a] != '\0')
-{
-if (n[a] >= 'a' && n[a] <= 'z')
-n[a] = n[a] - 32;
-a++;
-} return (
-n);
+	{
+		if (n[a] >= 'a' && n[a] <= 'z')
+		n[a] = n[a] - 32;
+		a++;
+	} 
+return (n);
 }
