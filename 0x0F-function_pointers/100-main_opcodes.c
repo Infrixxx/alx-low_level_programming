855ae02d31be40223868911cb3196b0e26c9b6d1
Boitumelo <bcrachoshi@gmail.com>
2023-04-19 18:41:42 -0700

    fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,45 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * print_opcodes - prints the opcodes of its own main function
- * @n: number of bytes to print
- */
-void print_opcodes(int n)
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
+int main(int argc, char *argv[])
 {
-	char *addr = (char *)print_opcodes;
-	int i;
+	int i, n;
 
-	for (i = 0; i < n; i++)
-		printf("%02x ", addr[i] & 0xff);
-	printf("\n");
-}
-
-/**
- * main - entry point
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, 1 if argc is incorrect, 2 if n is negative
- */
-int main(int argc, char **argv)
-{
-	int n;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	n = atoi(argv[1]);
-
 	if (n < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
-	print_opcodes(n);
-
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 	return (0);
 }
