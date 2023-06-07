bbd23e1ef6d6e44c211ac791bb52d093147c2f96
Boitumelo <bcrachoshi@gmail.com>
2023-06-07 21:58:52 +0200

    fix to puchar

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,10 @@
 
 void _puts_recursion(char *s)
 {
-        if (*s == '\0')
+        while (*s != '\0')
         {
-                _putchar('\n');
+                _putchar(*s);
+		s++;
         }
-        _putchar(s++);
+        _putchar('\n');
 }
