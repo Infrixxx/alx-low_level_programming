b4b54bd3435b444b00276e4d5d859f3ae89e5998
Boitumelo <bcrachoshi@gmail.com>
2023-06-07 23:47:22 +0200

    fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,9 +3,9 @@
 /**
  * _strlen_recursion - string length calculator
  * @s : string with length to be determined
+ *
  * Return : length of string
  */
-
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
