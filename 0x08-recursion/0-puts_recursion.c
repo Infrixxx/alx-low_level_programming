a67c1d9096e6a39c24336bc8dac1cef697a051f5
Boitumelo <bcrachoshi@gmail.com>
2023-06-07 22:02:56 +0200

    puts fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,14 @@
  * _puts_recursion - prints a string, followed by a new line.
  * @s: pointer to the string to be printed
  */
-
 void _puts_recursion(char *s)
 {
-        while (*s != '\0')
-        {
-                _putchar(*s);
-		s++;
-        }
-        _putchar('\n');
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
 }
