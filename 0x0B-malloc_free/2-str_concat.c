4fc24aa806f19ad85cbd1731f3dc868ac51acdb4
Boitumelo <bcrachoshi@gmail.com>
2023-06-13 19:30:52 +0200

    fix to string concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,27 +9,25 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-        char *s;
-        int a, b, c, d;
+	char *s;
+	int a, b, c, d;
 
-        if (s1 == 0)
-                s1 = "";
-        if (s2 == 0)
-                s2 = "";
-        for (a = 0; s1[a] != '\0'; a++)
-                ;
-        for (b = 0; s2[b] != '\0'; b++)
-                ;
-        b++;
-        s = malloc((a * sizeof(*s1)) + (b * sizeof(*s2)));
-        if (s == 0)
-                return (NULL);
-        for (c = 0, d = 0; c < a + b; c++)
-        {
-                if (c < a)
-                        s[c] = s1[c];
-                else
-                        s[c] = s2[d++];
-        }
-        return (s);
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+	for (a = 0; s1[a] != '\0'; a++);
+	for (b = 0; s2[b] != '\0'; b++);
+	b++;
+	s = malloc((a * sizeof(*s1)) + (b * sizeof(*s2)));
+	if (s == 0)
+		return (NULL);
+	for (c = 0, d = 0; c < a + b; c++)
+	{
+		if (c < a)
+			s[c] = s1[c];
+		else
+			s[c] = s2[d++];
+	}
+	return (s);
 }
