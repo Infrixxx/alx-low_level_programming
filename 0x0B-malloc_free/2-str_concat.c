19a84b162f6419a00f724cb98736e97e1a771efc
Boitumelo <bcrachoshi@gmail.com>
2023-06-13 19:35:53 +0200

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,11 +16,9 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == 0)
 		s2 = "";
-	
 	for (a = 0; s1[a] != '\0'; a++);
 	for (b = 0; s2[b] != '\0'; b++);
 	b++;
-	
 	s = malloc((a * sizeof(*s1)) + (b * sizeof(*s2)));
 	if (s == 0)
 		return (NULL);
