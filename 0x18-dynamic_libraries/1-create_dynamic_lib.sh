24f0c5a718484b1c0aea1e04918ebdaba3bb1b11
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-09-19 16:32:20 +0200

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 gcc -fPIC -c *.c
 gcc -shared -o liball.so *.o
+
+# Check for compilation errors
+if [ $? -eq 0 ]; then
+  echo "All functions found :)"
+  exit 0
+else
+  echo "Compilation failed :("
+  exit 1
+fi
